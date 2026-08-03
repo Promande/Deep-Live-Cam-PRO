5a6263603b1e2fa1f9c4c14150d50ce899b651cb
SD-Pro-Web <streamcasterpro@atomicmail.io>
2026-08-04 01:08:02 +0800

    Add localization files and GitHub workflow

---
+++ tkinter_fix.py
@@ -0,0 +1,21 @@
+import os
+os.environ.setdefault('TK_SILENCE_DEPRECATION', '1')
+
+import tkinter
+
+def apply_patch():
+    original_init = tkinter.Tk.__init__
+    
+    def patched_init(self, *args, **kwargs):
+        original_init(self, *args, **kwargs)
+        self.tk.eval("""
+        if {[info commands ::tk::ScreenChanged] == ""} {
+            proc ::tk::ScreenChanged {args} {
+                return
+            }
+        }
+        """)
+    
+    tkinter.Tk.__init__ = patched_init
+
+apply_patch()
